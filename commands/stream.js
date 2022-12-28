3752c8d703c3ed01fad950730600f3b0f546482a
SwyftPain <trejlerrs@gmail.com>
2022-12-28 05:57:04 +0100

    Update stream.js

--- commands/stream.js
+++ commands/stream.js
@@ -5,6 +5,7 @@ var configs = require("../config/databaseConfig");
 var connection = configs.connection;
 var { config } = require("../config/variables"); // get variables
 const myfunc = require("../config/functions"); // get functions
+const Eris = require("eris-additions")(require("eris"))
 
 class stream extends CommandStructure {
   constructor(client) {
@@ -55,53 +56,33 @@ class stream extends CommandStructure {
       return message.channel.createMessage(exampleEmbed.create);
     connection.query(
       `SELECT * from players WHERE Discordid="${message.author.id}"`,
-      function (error, results, fields) {
+      async function (error, results, fields) {
         if (error) throw error;
         if (results && results.length) {
           // store current amount of items
           let items = {
-            Level: results[0].Level,
+            Platform: results[0].Platform,
+            Status: results[0].Status,
             Money: results[0].Money,
-            Xp: results[0].Xp,
-            Xpneeded: results[0].Xpneeded,
             Life: results[0].Energy,
             Maxlife: results[0].Maxenergy,
-            Fans: results[0].Fans,
-            Device: results[0].Device,
+            Games: results[0].Games,
+            Subs: results[0].Subs
           };
           // minimum and maximum values
           let chances = {
-            Fans: { min: 1, max: 3 },
-            Coins: {
-              min: ~~Math.floor((items.Level / 0.4) ^ 10),
-              max: ~~Math.floor((items.Level / 0.7) ^ 15),
-            },
-            Xp: {
-              min: ~~Math.floor((items.Level / 0.2) ^ 6),
-              max: ~~Math.floor((items.Level / 0.3) ^ 8),
-            },
+            Subs: { min: 1, max: 3 },
             Lostlife: { min: items.Level + 1, max: items.Level * 5 },
-            Fanchance: { min: 1, max: 100 },
+            Subchance: { min: 1, max: 100 },
+            Coins: { min: 1, max: 5 }
           };
-          // increase fan amounts if device is set to phone
-          var currentdevice = items.Device;
-          switch (true) {
-            case currentdevice == "PHONE":
-              chances.Fans.min = 2;
-              chances.Fans.max = 5;
-              break;
-            default:
-              chances.Fans.min = 1;
-              chances.Fans.max = 3;
-          }
           // random number in range
           let generate = {
-            fans: myfunc.getRandom(chances.Fans.min, chances.Fans.max),
+            Subs: myfunc.getRandom(chances.Subs.min, chances.Subs.max),
             coins: myfunc.getRandom(chances.Coins.min, chances.Coins.max),
-            xp: myfunc.getRandom(chances.Xp.min, chances.Xp.max),
-            fanchance: myfunc.getRandom(
-              chances.Fanchance.min,
-              chances.Fanchance.max
+            Subchance: myfunc.getRandom(
+              chances.Subchance.min,
+              chances.Subchance.max
             ),
             lostlife: myfunc.getRandom(
               chances.Lostlife.min,
@@ -110,24 +91,22 @@ class stream extends CommandStructure {
           };
           // new amount
           let newvalue = {
-            Fans: items.Fans + generate.fans,
+            Subs: items.Subs + generate.Subs,
             Coins: items.Money + generate.coins,
-            Xp: items.Xp + generate.xp,
             Life: items.Life - generate.lostlife,
-            Maxlife: items.Maxlife + 5,
-            Xpneeded: ~~Math.floor((items.Level / 0.03) ^ 5),
+            Maxlife: items.Maxlife + 5
           };
           // the message to display based on whether the amount is singular or plural
           class Print {
-            constructor(fans, coins) {
-              this.fans = fans;
+            constructor(Subs, coins) {
+              this.Subs = Subs;
               this.coins = coins;
             }
           }
-          const print = new Print("followers", "coins");
+          const print = new Print("subs", "coins");
           var thestreammessage;
-          if (generate.fans == "1") {
-            print.fans = "follower";
+          if (generate.Subs == "1") {
+            print.Subs = "sub";
           }
           if (generate.coins == "1") {
             print.coins = "coin";
@@ -142,117 +121,8 @@ class stream extends CommandStructure {
             // new values after death
             let dead = {
               Level: items.Level - 1,
-              Xpneeded: items.Xpneeded / 2,
               Maxlife: items.Maxlife - 5,
             };
-            switch (true) {
-              // can lose levels
-              case dead.Level > 0:
-                var sql = `UPDATE players SET Energy = '1', Level = '${dead.Level}', Xpneeded = '${dead.Xpneeded}', Xp = '0', Maxenergy = '${dead.Maxlife}' WHERE Discordid = '${message.author.id}'`;
-                connection.query(sql, function (err, result) {
-                  if (err) throw err;
-                  cmdCD.addCoolDown(
-                    message.author.id,
-                    config.cooldown.stream,
-                    "cmd-stream"
-                  );
-                  setTimeout(reminder, config.cooldown.stream);
-                  const streamEmbed = new MessageEmbed()
-                    .setColor(config.color.error)
-                    .setDescription(
-                      `Restore your energy before you can stream again.\nYou lost a level too.`
-                    )
-                    .setTitle(`YOU PASSED OUT!`)
-                    .setThumbnail(`${message.author.avatarURL}`);
-                  return message.channel.createMessage(streamEmbed.create);
-                });
-                break;
-              default:
-                // cant lose levels
-                var sql = `UPDATE players SET Energy = '1' WHERE Discordid = '${message.author.id}'`;
-                connection.query(sql, function (err, result) {
-                  if (err) throw err;
-                  cmdCD.addCoolDown(
-                    message.author.id,
-                    config.cooldown.stream,
-                    "cmd-stream"
-                  );
-                  setTimeout(reminder, config.cooldown.stream);
-                  const streamEmbed = new MessageEmbed()
-                    .setColor(config.color.error)
-                    .setDescription(
-                      `Restore your energy before you can stream again.`
-                    )
-                    .setTitle(`YOU PASSED OUT!`)
-                    .setThumbnail(`${message.author.avatarURL}`);
-                  return message.channel.createMessage(streamEmbed.create);
-                });
-                break;
-            }
-          } else {
-            // if user is not dying
-            switch (true) {
-              case generate.fanchance < 30:
-                // if user gets any fans
-                var sql = `UPDATE players SET Fans = '${newvalue.Fans}', Money = '${newvalue.Coins}', Xp = '${newvalue.Xp}', Energy = '${newvalue.Life}' WHERE Discordid = '${message.author.id}'`;
-                connection.query(sql, function (err, result) {
-                  if (err) throw err;
-                });
-                thestreammessage = `**${current.user.tag}** went streaming and got ${generate.fans} **${print.fans}**.\n**${current.user.tag}** got ${generate.coins} **${print.coins}** and ${generate.xp} **XP**.\n**${current.user.tag}** lost ${generate.lostlife} **energy** and now has ${newvalue.Life}/${items.Maxlife} **energy**.`;
-                break;
-              default:
-                // if user does not get any fans
-                var sql = `UPDATE players SET Money = '${newvalue.Coins}', Xp = '${newvalue.Xp}', Energy = '${newvalue.Life}' WHERE Discordid = '${message.author.id}'`;
-                connection.query(sql, function (err, result) {
-                  if (err) throw err;
-                });
-                thestreammessage = `**${current.user.tag}** went streaming.\n**${current.user.tag}** got ${generate.coins} **${print.coins}** and ${generate.xp} **XP**.\n**${current.user.tag}** lost ${generate.lostlife} **energy** and now has ${newvalue.Life}/${items.Maxlife} **energy**.`;
-            }
-            if (newvalue.Xp >= items.Xpneeded) {
-              // check if new xp is higher than the needed xp for level up
-              let levelup = {
-                Level: items.Level + 1,
-                Life: newvalue.Maxlife,
-              };
-              const lvlEmbed = new MessageEmbed()
-                .setColor(config.color.levelup)
-                .setDescription(
-                  `**${current.user.tag} LEVELED UP!**.\n**${current.user.tag}** got energy restored.`
-                )
-                .setTitle(`:fire: LEVEL UP`)
-                .setThumbnail(`${message.author.avatarURL}`);
-              var sqls = `UPDATE players SET Level = '${levelup.Level}', Maxenergy = '${newvalue.Maxlife}', Xp = '0', Energy = '${newvalue.Maxlife}', Xpneeded = '${newvalue.Xpneeded}' WHERE Discordid = '${message.author.id}'`;
-              connection.query(sqls, function (err, result) {
-                if (err) throw err;
-                return message.channel.createMessage(lvlEmbed.create);
-              });
-              cmdCD.addCoolDown(
-                message.author.id,
-                config.cooldown.stream,
-                "cmd-stream"
-              ); // add cooldown
-              setTimeout(reminder, config.cooldown.stream); // remind when cooldown is over
-              // level up message
-              const streamEmbed = new MessageEmbed()
-                .setColor(config.color.standard)
-                .setDescription(`${thestreammessage}`)
-                .setTitle(`:red_circle: LIVE NOW`)
-                .setThumbnail(`${message.author.avatarURL}`);
-              return message.channel.createMessage(streamEmbed.create);
-            }
-            /* if (jailchance < 5) {
-              const streamEmbede = new MessageEmbed()
-                .setColor(config.color.standard)
-                .setDescription(
-                  `This message is a placeholder for antispam check. You can safely ignore it for now until further development.\nThis will be a multiple option question.`
-                )
-                .setTitle(
-                  `:police_officer::skin-tone-1: Your documents please!`
-                )
-                .setThumbnail(`${message.author.avatarURL}`);
-              return message.channel.createMessage(streamEmbede.create);
-            } */
-            // display stream message
             cmdCD.addCoolDown(
               message.author.id,
               config.cooldown.stream,
@@ -268,11 +138,15 @@ class stream extends CommandStructure {
           }
         } else {
           // if the user executing the command does not exist in the database, add it to the database
-          var sql = `INSERT INTO players (Title, Medicine, Discordid, Played, Fans, Level, Money, Xp, Xpneeded, Energy, Maxenergy, Dname, Moder, Games, Replays, Subs, Antivirus, Device, Vpn, Credit) VALUES (' ', '0', '${message.author.id}', '1', '1', '1', '0', '0', '10', '100', '100','${message.author.username}', '0', '0', '0', '0', '0', 'NONE', 'NONE', '0')`;
+          var sql = `INSERT INTO players (Discordid, Platform, Status, Money, Energy, Maxenergy, Title, Dname, Moder, Games, Subs) VALUES ('${message.author.id}', 'None', 'Regular', '0', '100', '100', '', '${message.author.username}', '0', '0', '0')`;
           connection.query(sql, function (err, result) {
             if (err) throw err;
           });
           help();
+          message.channel.createMessage("Choose  your platform:\n\n1. Twitch\n2. Youtube");
+          let responses = await message.channel.awaitMessages(m => m.content === "yes", { time: 10000, maxMatches: 1 });
+if(responses.length) message.channel.createMessage("You said yes :)");
+else message.channel.createMessage("You didn't say yes :(");
         }
       }
     );
